81b38d668625a767008f7b277b9fb878462519f6
mimi5930 <mikej.miller440@gmail.com>
2022-04-22 21:07:35 -0500

    created user, book, and auth types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,54 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
+
+  # Queries
   type Query {
     me: User
   }
 
+  # Mutations
   type Mutation {
+    # logs in user. Email and password required. Returns Auth
     login(email: String!, password: String!): Auth
+    # Adds new user. username, email, and password required. returns auth
     addUser(username: String!, email: String!, password: String!): Auth
     # TODO: add saveBook parameters (look into input type)
     saveBook(): User
+    # removes book by bookId
     removeBook(bookId: ID!)
   }
 
-  type User
+  # User
+  type User {
+    _id
+    username
+    email
+    password
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+
+# Book
+type Book {
+  bookId
+  authors
+  description
+  title
+  image
+  link
+}
+
+# Auth
+type Auth {
+  token
+  user
+}
 `;
