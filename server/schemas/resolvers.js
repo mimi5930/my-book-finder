efa47e6760f3d832636210dd58d8350b27d1455a
mimi5930 <mikej.miller440@gmail.com>
2022-04-22 21:07:45 -0500

    created a query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,20 @@
 import { AuthenticationError } from 'apollo-server-express';
+import { User } from '../models';
+import { signToken } from '../utils/auth';
 
-export const resolvers = {};
+// resolvers
+export const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const findUser = await User.findOne({ _id: context.user._id })
+          .select('-__v, -password')
+          .populate('savedBooks');
+
+        return findUser;
+      }
+
+      throw new AuthenticationError('Please Log in');
+    }
+  }
+};
