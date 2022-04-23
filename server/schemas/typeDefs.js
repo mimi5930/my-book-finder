b114794cf345e2755a6b7f4733316f960098f1da
Mike <mikej.miller440@gmail.com>
2022-04-22 20:38:24 -0500

    created query and mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,17 @@
 import { gql } from 'apollo-server-express';
 
-export const typeDefs = gql``;
+export const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    # TODO: add saveBook parameters (look into input type)
+    saveBook(): User
+    removeBook(bookId: ID!)
+  }
+
+  type User
+`;
