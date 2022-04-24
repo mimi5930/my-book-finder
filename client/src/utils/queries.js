17e318a8b4c6e7cb68e81334b9e8e00edce502ce
Mike <mikej.miller440@gmail.com>
2022-04-24 16:17:20 -0500

    got rid of password field

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,11 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  {
+  query me {
     me {
       _id
       username
       email
-      password
       bookCount
       savedBooks {
         bookId
