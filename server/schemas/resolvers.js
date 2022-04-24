dc138e1c85c1d19b2030561e1ed15d3033c98d15
Mike <mikej.miller440@gmail.com>
2022-04-24 11:40:20 -0500

    added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,9 @@ const { signToken } = require('../utils/auth');
 
 // resolvers
 const resolvers = {
+  // Queries
   Query: {
+    // return logged in user
     me: async (parent, args, context) => {
       if (context.user) {
         const findUser = await User.findOne({ _id: context.user._id })
@@ -14,7 +16,35 @@ const resolvers = {
         return findUser;
       }
 
+      // if JWT is not valid for user
       throw new AuthenticationError('Please Log in');
+    }
+  },
+
+  // Mutations
+  Mutation: {
+    // log in user with email and password
+    login: async (parent, { email, password }) => {
+      const findUser = await User.findOne({ email });
+
+      // if no user is found with email
+      if (!findUser) {
+        throw new AuthenticationError('The email or password is incorrect!');
+      }
+
+      // bcrypt compare passwords
+      const checkPw = await user.isCorrectPassword(password);
+
+      // if passwords do not match
+      if (!checkPw) {
+        throw new AuthenticationError('The email or password is incorrect!');
+      }
+
+      // assign JWT
+      const newToken = signToken();
+
+      // return user and token
+      return { findUser, newToken };
     }
   }
 };
